ef60c39730fc059f7b4c92d472fd2eeceafd4650
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-12 01:42:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 # Add  code here!
-def prime?(num)
-  if num <= 1
-    return false
-  elsif
-    num.each do |i|
+def prime?(number)
+  return false if number < 2
+  return false if !number.integer?
+  return true if number = 2
+  (2..number-1).each {|int| returns false if number % int == 0}
+  true
+end
   
     
     
