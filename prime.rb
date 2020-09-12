517da1ffa50acf66d9ff00989c1566ed7877462b
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-12 01:43:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(number)
   return false if number < 2
   return false if !number.integer?
   return true if number = 2
-  (2..number-1).each {|int| returns false if number % int == 0}
+  (2..number-1).each {|int| return false if number % int == 0}
   true
 end
   
