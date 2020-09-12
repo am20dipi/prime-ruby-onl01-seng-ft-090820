16f3d991d128054d91c926039aefaf89d25a0666
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-12 01:44:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(number)
+  return false if number < 2
+  return false if !number.integer?
+  return true if number == 2
+  (2..number-1).each {|int| return false if number % int == 0}
+  true
+end
+  
+puts prime?(19)    
+puts prime?(8)   
